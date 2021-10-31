d785095ed969c8a473528ec50b0ade0b416bfcd7
faisalmirji123 <faisalmirji@gmail.com>
2021-10-31 17:58:49 +0530

    2 files modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-// import React from 'react';
-
 import React from 'react';
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar } from 'react-bootstrap'
 
 function App(){
   return(
     <div className="App">
+      <Navbar>
+        </Navbar>
       </div>
   )
 }
