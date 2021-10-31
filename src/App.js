c7708bf2b365c9a62310ad39065497b1bc17d5d0
faisalmirji123 <faisalmirji@gmail.com>
2021-10-31 18:43:59 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Navbar } from 'react-bootstrap'
 
 function App(){
   return(
-    <div className="App">
+    <div className="App">HI
       <Navbar bg="orange" variant="dark">
         <Navbar.Brand>
           <img src="./images/photos.jpg" alt=""/>
