64f95053649d4a16e3463297e0d71e32751d0d91
faisalmirji123 <faisalmirji@gmail.com>
2021-10-31 17:40:03 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-import React from "react";
-import "./style.css";
+// import React from 'react';
+import App from  "./src/Components/App";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
-}
