ed582d9bab3c20608854e3bdf59aab8f095ba20e
faisalmirji123 <faisalmirji@gmail.com>
2021-11-04 16:16:45 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ import { Nav, Navbar } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <Navbar bg="myRed" variant="dark">
+      <Navbar bg="dark" variant="dark">
         <Navbar.Brand>
           <img src={'./images/plants.jpg'} />
-            
         </Navbar.Brand>
         <Nav>
-          <Nav.Link className="About"><h2>About</h2></Nav.Link>
-          <Nav.Link className="Events"><h2>Events</h2> </Nav.Link>
-          <Nav.Link className="Volunteers"><h2>Volunteers</h2> </Nav.Link>
-          <Nav.Link className="Donations"><h2>Donations</h2> </Nav.Link>
-          <Nav.Link className="Sign "><h2>Login/Register</h2></Nav.Link>
+          <Nav.Link className="About">
+            <h2>About</h2>
+          </Nav.Link>
+          <Nav.Link className="Events">
+            <h2>Events</h2>{' '}
+          </Nav.Link>
+          <Nav.Link className="Volunteers">
+            <h2>Volunteers</h2>{' '}
+          </Nav.Link>
+          <Nav.Link className="Donations">
+            <h2>Donations</h2>{' '}
+          </Nav.Link>
+          <Nav.Link className="Sign ">
+            <h2>Login/Register</h2>
+          </Nav.Link>
         </Nav>
       </Navbar>
     </div>
