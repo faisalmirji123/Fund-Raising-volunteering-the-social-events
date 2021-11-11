60cc1388aa443dbb9a593669d2e9d1531fd19cac
faisalmirji123 <faisalmirji@gmail.com>
2021-11-11 22:02:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -23,9 +23,7 @@ function App() {
           <Nav.Link className="Donations">
             <h2>Donations</h2>{' '}
           </Nav.Link>
-          <Nav.Link className="Donations">
-            <h2>Donations</h2>{' '}
-          </Nav.Link>
+
           <Nav.Link className="Sign ">
             <h2>Login/Register</h2>
           </Nav.Link>
