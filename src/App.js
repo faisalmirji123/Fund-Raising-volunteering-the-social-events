3a30ff67c8d55132be53d332fad4aec2dfbca5d2
faisalmirji123 <faisalmirji@gmail.com>
2021-11-22 22:41:23 +0530

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-// import React from 'react';
-// import './style.css';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import { Nav, Navbar } from 'react-bootstrap';
+import React from 'react';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Nav, Navbar } from 'react-bootstrap';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar bg="dark" variant="light">
-//         <Navbar.Brand>
-//           hello
-//         </Navbar.Brand>
-//         <Nav>
-//           <Nav.Link className="About">
-//             <h2>About</h2>
-//           </Nav.Link>
-//           <Nav.Link className="Events">
-//             <h2>Events</h2>{' '}
-//           </Nav.Link>
-//           <Nav.Link className="Volunteers">
-//             <h2>Volunteers</h2>{' '}
-//           </Nav.Link>
-//           <Nav.Link className="Donations">
-//             <h2>Donations</h2>{' '}
-//           </Nav.Link>
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="light">
+        <Navbar.Brand>
+          hello
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Link className="About">
+            <h2>About</h2>
+          </Nav.Link>
+          <Nav.Link className="Events">
+            <h2>Events</h2>{' '}
+          </Nav.Link>
+          <Nav.Link className="Volunteers">
+            <h2>Volunteers</h2>{' '}
+          </Nav.Link>
+          <Nav.Link className="Donations">
+            <h2>Donations</h2>{' '}
+          </Nav.Link>
 
-//           <Nav.Link className="Sign ">
-//             <h2>Login/Register</h2>
-//           </Nav.Link>
-//         </Nav>
-//       </Navbar>
-//       <br />
-//     </div>
-//   );
-// }
-// export default App;
+          <Nav.Link className="Sign ">
+            <h2>Login/Register</h2>
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <br />
+    </div>
+  );
+}
+export default App;
