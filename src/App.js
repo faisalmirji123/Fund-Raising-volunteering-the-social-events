d566b568fa3fd3def3f23bfac0bd4920e6a36712
faisalmirji123 <faisalmirji@gmail.com>
2021-11-23 23:30:50 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Navbar bg="dark" variant="light">
         <Navbar.Brand>
-          hello
+          hEY
         </Navbar.Brand>
         <Nav>
           <Nav.Link className="About">
